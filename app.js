59b6cb4182a8a46ca0731da26a66d4d6e25d4bd6
Vikram Singh <168630325+vise07cs@users.noreply.github.com>
2025-08-28 09:08:14 +0530

    resApp+wildcard character

--- app.js
+++ app.js
@@ -3,28 +3,32 @@ const express=require("express");
 const app=express();
 
 
-app.get("/orders",(req,res)=>{
-  res.send("<h1>Here is the list of all orders. </h1>")
+app.get("/products",(req,res)=>{
+  res.send("<h1>Here is the list of all products. </h1>")
 })
 
 
-app.post("/orders",(req,res)=>{
-  res.send("<h1>A new order has been created </h1>")
+app.post("/products",(req,res)=>{
+  res.send("<h1>A new product has been added </h1>")
 })
 
 
-app.get("/users",(req,res)=>{
-  res.send("<h1> Here is the list of all users. </h1>")
+app.get("/categories",(req,res)=>{
+  res.send("<h1> Here is the list of all categories. </h1>")
 })
 
 
-app.post("/user",(req,res)=>{
-res.send("<h1>A new user has been added.</h1>")
+app.post("/categories",(req,res)=>{
+res.send("<h1>A new category has been added.</h1>")
+})
+
+app.all("/:any",(req,res)=>{
+  res.status(404).send("<h1>404 - Page Not Found</h1>")
 })
 
 
-app.listen(3000,()=>{
-    console.log("Server is up and running on port 3000.");
+app.listen(4000,()=>{
+    console.log("Server is up and running on port 4000.");
 });
 
 
